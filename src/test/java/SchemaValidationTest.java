7e8ba1bf3b5519e928b518ae228e116d1a281f37
Łukasz Wojtala <lukaszwojtala1997@gmail.com>
2023-11-09 22:00:26 +0100

    Verifying JSON Schema

---
+++ src/test/java/SchemaValidationTest.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.when;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class SchemaValidationTest {
+
+    @Test
+    public void getPost(){
+
+                when().
+                        get("http://localhost:3000/posts/{postId}", 1).
+                then().
+                        assertThat().body(matchesJsonSchemaInClasspath("postSchema.json"));
+    }
+}
