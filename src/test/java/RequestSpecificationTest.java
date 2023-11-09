0096c32d316de953ecf77dc1cec9f7448e41d450
Łukasz Wojtala <lukaszwojtala1997@gmail.com>
2023-11-09 22:02:54 +0100

    Creating new test scenarios

---
+++ src/test/java/RequestSpecificationTest.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class RequestSpecificationTest extends BaseTest{
+
+    @Test
+    public void getPosts(){
+        given().
+                spec(requestSpecification).
+        when().
+                get().
+        then().
+                spec(responseSpecification);
+    }
+
+    @Test
+    public void getPost(){
+        given().
+                spec(requestSpecification).
+        when().
+                get("/1").
+        then().
+                spec(responseSpecification);
+    }
+}
